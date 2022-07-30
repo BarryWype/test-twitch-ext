b8b76577e87e8b88d737a9d71fbc31940ef83e62
Barry <barrywype@gmail.com>
2022-07-30 22:39:50 +1200

    add config

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const { VueLoaderPlugin } = require('vue-loader')
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, "./public"),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            // ... other rules
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loader: "file-loader",
+                options: {
+                    name: "img/[name].[ext]"
+                }
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin!
+        new VueLoaderPlugin(),
+        new Dotenv()
+    ]
+}
