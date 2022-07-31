a74738e3dc1d5f58beab7913328a99988cb9506b
Samy Poiraud <samypoiraud@gmail.com>
2022-07-31 02:52:13 +0200

    add steamapi test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const twitch = require('../service/twitch')
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI('99C5FC93E5666087F61A95A483B4416C');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,4 +10,57 @@ router.get('/', function (req, res, next) {
   res.json(twitch.colorCycleHandler(req))
 });
 
-module.exports = router
+router.get('/user', function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  steam.resolve('https://steamcommunity.com/profiles/76561198020613214/').then(id => {
+    console.log(id);
+    steam.getUserSummary(id).then(summary => {
+      console.log(summary);
+      res.end(JSON.stringify({ a: summary }));
+    });
+  }).catch(() => {
+    res.end(JSON.stringify({ a: "error" }));
+  });
+});
+
+router.get('/apps', function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  steam.resolve('https://steamcommunity.com/profiles/76561198020613214/').then(id => {
+    console.log(id);
+    steam.getUserOwnedGames(id).then(apps => {
+      console.log(apps);
+      res.end(JSON.stringify({ res: apps }));
+    })
+  }).catch(() => {
+    res.end(JSON.stringify({ a: "error" }));
+  });
+});
+
+router.get('/achivement', function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  steam.resolve('https://steamcommunity.com/profiles/76561198020613214/').then(id => {
+    console.log(id);
+    steam.get(`/ISteamUserStats/GetPlayerAchievements/v1?steamid=${id}&appid=${req.query.appId}&l=french`).then(success => {
+      console.log(success);
+      res.end(JSON.stringify({ res: success }));
+    })
+  }).catch(() => {
+    res.end(JSON.stringify({ a: "error" }));
+  });
+});
+
+router.get('/game', function (req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  steam.resolve('https://steamcommunity.com/profiles/76561198020613214/').then(id => {
+    console.log(id);
+    steam.getGameSchema(req.query.appId).then(success => {
+      console.log(success);
+      res.end(JSON.stringify({ res: success }));
+    })
+  }).catch(() => {
+    res.end(JSON.stringify({ a: "error" }));
+  });
+});
+
+
+module.exports = router;
